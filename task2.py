cd8f67a35644b907a5760e930d1fe331a83ea9c3
Elias Nunes <eliasn510@deltalearns.ca>
2021-10-13 14:06:20 -0700

    1

---
+++ task2.py
@@ -0,0 +1,26 @@
+#! python3
+"""
+###### Task 2
+Ask the user to enter a name.
+Check the name against a tuple that contains a series of names to see if it is a match. Use a for loop this time instead of a single if with multiple
+logical operators
+(2 points)
+inputs:
+str name
+outputs:
+"That name is in the list"
+"That name is not in the list"
+example:
+Enter a name: Grace
+That name is not on the list
+example:
+Enter a name: Lebron
+That name is on the list
+"""
+name = str(input("Enter your name:")).strip()
+nameList = ("Lebron","Kobe","Michale","Shaq","Dennis") 
+if name in nameList:
+    print("That name is on the list")
+else:
+    print("That name is not on the list")
+    
